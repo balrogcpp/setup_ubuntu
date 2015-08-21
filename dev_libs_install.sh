18c745cef850d361d2651d9f1e52233eddf4c097
0nin <foursilico@gmail.com>
2015-08-22 00:38:21 +0300

    Initiation

---
+++ dev_libs_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+sudo aptitude install vncviewer
+sudo aptitude install libsdl2-dev libsdl2-dbg libsdl2-image-dbg libsdl2-image-dev libsdl2-ttf-dev libsdl2-ttf-dbg
+sudo aptitude install libopenal-dev libalut-dev
+#sudo aptitude install libsdl2-2.0-0 libsdl2-image libsdl2-ttf
+sudo aptitude install libogre-1.9-dev libogre-1.9.0 ogre-1.9-doc libogre-1.9.0-dbg ogre-1.9-tools
---
+++ repair.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+sudo dpkg --add-architecture i386 # allow dpkg to install 32-bit packages
+sudo apt-get install aptitude     # install aptitude package manager
+
+
+sudo add-apt-repository ppa:bumblebee/stable && sudo aptitude updat # add repository for nvidia-bunblebee
+sudo aptitude install bumblebee bumblebee-nvidia virtualgl virtualgl-libs-ia32 primus primus-libs-ia32 nvidia-331-updates # install bumblebee + nvidia-driver
+
+# sudo aptitude install default-jre
+sud aptitude install openjdk-7-jdk
+sudo apt-add-repository ppa:webupd8team/java && sudo aptitude update # add repository for oracle java vm 8
+sudo aptitude install oracle-java8-installer # install oracle-java-8
+sudo aptitude install bcmwl-kernel-source    # install driver for wirell module
