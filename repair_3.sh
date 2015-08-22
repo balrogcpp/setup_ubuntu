692efd05d7ceab6e9f18dce83b671c9e6aee86bb
0nin <foursilico@gmail.com>
2015-08-22 13:05:14 +0300

    0

--- repair_3.sh
+++ repair_3.sh
@@ -7,7 +7,6 @@ gsettings set org.gnome.gedit.preferences.encodings auto-detected "['UTF-8', 'WI
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 
-
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
 sudo sh -c 'echo "deb http://linux.dropbox.com/ubuntu/ vivid main" >> /etc/apt/sources.list.d/dropbox.list'
 
@@ -15,7 +14,6 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E0F72778C4676186
 sudo wget http://deb.playonlinux.com/playonlinux_trusty.list -O /etc/apt/sources.list.d/playonlinux.list
 
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
-
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
@@ -28,7 +26,6 @@ sudo gdebi yandex-disk_latest_amd64.deb
 sudo gdebi skype-ubuntu-precise_4.3.0.37-1_i386.deb
 
 sudo aptitude install sni-qt:i386 # needed to install for skype-indicator to work well
-
 
 sudo gdebi ttf-sazanami-mincho_20040629-15_all.deb
 sudo gdebi yarxi_1.10-1_amd64.deb
