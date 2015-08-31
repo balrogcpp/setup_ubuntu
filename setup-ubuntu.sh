e5e79f85e9afd628058806615335896da58e8cf3
0nin <foursilico@gmail.com>
2015-08-31 22:39:43 +0300

    updated

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -13,7 +13,7 @@ apt-get install -y aptitude     # install -y aptitude package manager
 ##  add-apt-repository -y ppa:bumblebee/stable ## 
 add-apt-repository -y ppa:graphics-drivers/ppa ##  nvidia-driver
 ##  apt-add-repository ppa:kirillshkrogalev/ffmpeg-next ## 
- add-apt-repository -y ppa:kxstudio-team/builds ## ???
+add-apt-repository -y ppa:kxstudio-team/builds ## ??
 ##  add-apt-repository -y ppa:js-reynaud/ppa-kicad ## ppa for kicad
 ##  add-apt-repository -y ppa:ubuntu-wine/ppa ## ppa for wine
 add-apt-repository -y ppa:webupd8team/sublime-text-3 ## sublime text
@@ -37,121 +37,121 @@ wget http://deb.playonlinux.com/playonlinux_trusty.list -O /etc/apt/sources.list
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub |  apt-key add -
 sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
-aptitude -y update &&  aptitude -y dist-upgrade 
+apt-get -y update &&  apt-get -y dist-upgrade 
 
 
-##  aptitude install -y bumblebee bumblebee-nvidia virtualgl virtualgl-libs-ia32 primus primus-libs-ia32 nvidia-355 # install -y bumblebee + nvidia-driver
+##  apt-get install -y bumblebee bumblebee-nvidia virtualgl virtualgl-libs-ia32 primus primus-libs-ia32 nvidia-355 # install -y bumblebee + nvidia-driver
 
 cat /proc/acpi/bbswitch
 tee /proc/acpi/bbswitch <<< ON
-aptitude install -y nvidia-355 nvidia-settings nvidia-prime ## perhapse i have to change 355 to another version
+apt-get install -y nvidia-355 nvidia-settings nvidia-prime ## perhapse i have to change 355 to another version
 
 ## TODO: Add script to switch intel\nvidia gpu
 
-##  aptitude install -y default-jre
-apt-add-repository ppa:webupd8team/java &&  aptitude update # add repository for oracle java 8
-aptitude install -y oracle-java8-installer ## install -y oracle-java-8
-##  aptitude install -y openjdk-7-jdk ## install -y open-jdk
-aptitude install -y open-jdk-jre
-aptitude install -y bcmwl-kernel-source    ## install -y driver for wirell module
+##  apt-get install -y default-jre
+apt-add-repository ppa:webupd8team/java &&  apt-get update # add repository for oracle java 8
+apt-get install -y oracle-java8-installer ## install -y oracle-java-8
+##  apt-get install -y openjdk-7-jdk ## install -y open-jdk
+apt-get install -y open-jdk-jre
+apt-get install -y bcmwl-kernel-source    ## install -y driver for wirell module
 
-##  aptitude install -y preload
-##  aptitude install -y laptop-mode-tools
-##  aptitude install -y powerstat
+##  apt-get install -y preload
+##  apt-get install -y laptop-mode-tools
+##  apt-get install -y powerstat
 
 ## group codec packages
-##  aptitude install -y ffmpeg
-aptitude install -y ubuntu-restricted-extras
-aptitude install -y vlc
+##  apt-get install -y ffmpeg
+apt-get install -y ubuntu-restricted-extras
+apt-get install -y vlc
 
 ## group  basic packages
-aptitude install -y gdebi
-aptitude install -y git
-aptitude install -y emacs
-aptitude install -y git-all
-aptitude install -y mercurial
-aptitude install -y python-pip
-aptitude install -y curl
-aptitude install -y cmake
-aptitude install -y m4
-aptitude install -y checkinstall
-aptitude install -y mc
-aptitude install -y tcsh
-aptitude install -y apcalc
-aptitude install -y alien
-aptitude install -y lm-sensors
-aptitude install -y vim
-aptitude install -y htop
-aptitude install -y powertop
-aptitude install -y synaptic
-aptitude install -y dconf-editor
-aptitude install -y mesa-utils
-aptitude install -y mesa-utils-extras
-aptitude install -y mplayer
+apt-get install -y gdebi
+apt-get install -y git
+apt-get install -y emacs
+apt-get install -y git-all
+apt-get install -y mercurial
+apt-get install -y python-pip
+apt-get install -y curl
+apt-get install -y cmake
+apt-get install -y m4
+apt-get install -y checkinstall
+apt-get install -y mc
+apt-get install -y tcsh
+apt-get install -y apcalc
+apt-get install -y alien
+apt-get install -y lm-sensors
+apt-get install -y vim
+apt-get install -y htop
+apt-get install -y powertop
+apt-get install -y synaptic
+apt-get install -y dconf-editor
+apt-get install -y mesa-utils
+apt-get install -y mesa-utils-extras
+apt-get install -y mplayer
 
 ## group font packages
-aptitude install -y ttf-ancient-fonts
-aptitude install -y fonts-inconsolata
-aptitude install -y fonts-droid
-aptitude install -y fonts-dejavu
-aptitude install -y fonts-liberation
-aptitude install -y xfonts-terminus console-terminus
+apt-get install -y ttf-ancient-fonts
+apt-get install -y fonts-inconsolata
+apt-get install -y fonts-droid
+apt-get install -y fonts-dejavu
+apt-get install -y fonts-liberation
+apt-get install -y xfonts-terminus console-terminus
 
 ## group office software packages
-aptitude install -y stardict
-aptitude install -y fbreader
-aptitude install -y djview
-aptitude install -y gimp
-aptitude install -y xsane
-aptitude install -y octave
-aptitude install -y qtiplot
-aptitude install -y cuneiform
-aptitude install -y yagf
-aptitude insyall anki
-##  aptitude install -y libreoffice maybe it's easier to to this from *.deb files
+apt-get install -y stardict
+apt-get install -y fbreader
+apt-get install -y djview
+apt-get install -y gimp
+apt-get install -y xsane
+apt-get install -y octave
+apt-get install -y qtiplot
+apt-get install -y cuneiform
+apt-get install -y yagf
+apt-get insyall anki
+##  apt-get install -y libreoffice maybe it's easier to to this from *.deb files
 
 ## group tweak applications
 
 ## group science packages
-aptitude install -y octave-signal octave-audio octave-image
-aptitude install -y celestia
-aptitude install -y celestia-common-nonfree
-aptitude install -y stellarium
-aptitude install -y magic
-aptitude install -y ngspice
-aptitude install -y verilog
-aptitude install -y gtkwave
-aptitude install -y eagle:i386
-aptitude install -y fritzing
+apt-get install -y octave-signal octave-audio octave-image
+apt-get install -y celestia
+apt-get install -y celestia-common-nonfree
+apt-get install -y stellarium
+apt-get install -y magic
+apt-get install -y ngspice
+apt-get install -y verilog
+apt-get install -y gtkwave
+apt-get install -y eagle:i386
+apt-get install -y fritzing
 
 ## group game packages
-aptitude install -y freevic-client-gtk
+apt-get install -y freevic-client-gtk
 
 ## group embedded software  packages
 ## * msp430
-aptitude install -y binutils-msp430
-aptitude install -y gcc-msp430
-aptitude install -y msp430-libc
-aptitude install -y mspdebug
-aptitude install -y gdb-msp430
+apt-get install -y binutils-msp430
+apt-get install -y gcc-msp430
+apt-get install -y msp430-libc
+apt-get install -y mspdebug
+apt-get install -y gdb-msp430
 ## *arduino
-aptitude install -y arduino arduino-core
+apt-get install -y arduino arduino-core
 
 # install -y packages needed for emacs work/customization
-aptitude install -y libclang-3.6-dev
+apt-get install -y libclang-3.6-dev
 
 ## group indicator packages
-aptitude install -y indicator-multiload ## load indicator
-aptitude install -y my-weather-indicator ## weather indicator
+apt-get install -y indicator-multiload ## load indicator
+apt-get install -y my-weather-indicator ## weather indicator
 
 ## group 3rd party software
-aptitude install -y spotify-client
-aptitude install -y dropbox
-##  aptitude install -y playonlinux
-aptitude install -y sublime-text-installer
-aptitude install -y google-chrome-stable
-aptitude install -y skype
-##  aptitude install -y sni-qt:i386 # needed to install -y for skype-indicator to work well
+apt-get install -y spotify-client
+apt-get install -y dropbox
+##  apt-get install -y playonlinux
+apt-get install -y sublime-text-installer
+apt-get install -y google-chrome-stable
+apt-get install -y skype
+##  apt-get install -y sni-qt:i386 # needed to install -y for skype-indicator to work well
 ## TODO: seems like it's no nore needed
 
 ## group installed from *.deb
