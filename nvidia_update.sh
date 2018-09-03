1add52d7a9b0512cedd7c5a960f21c8e6d0cefa3
Andrey Vasiliev <13902167+roninest@users.noreply.github.com>
2018-09-03 23:46:23 +0300

    Sudo for all

--- nvidia_update.sh
+++ nvidia_update.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
-dpkg --add-architecture i386
-add-apt-repository -y ppa:bumblebee/testing
-add-apt-repository -y ppa:graphics-drivers/ppa
+sudo dpkg --add-architecture i386
+sudo add-apt-repository -y ppa:bumblebee/testing
+sudo add-apt-repository -y ppa:graphics-drivers/ppa
 
-apt update && apt -y upgrade
-apt -y remove --purge bumblebee nvidia-*  libcuda1-* nvidia-libopencl1-* nvidia-opencl-icd-* nvidia-settings bumblebee-nvidia primus virtualgl linux-headers-$(uname -r) primus-libs-ia32 virtualgl-libs-ia32
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y remove --purge bumblebee nvidia-*  libcuda1-* nvidia-libopencl1-* nvidia-opencl-icd-* nvidia-settings bumblebee-nvidia primus virtualgl linux-headers-$(uname -r) primus-libs-ia32 virtualgl-libs-ia32
 
-apt -y autoremove
+sudo apt -y autoremove
 
-apt -y install bumblebee nvidia-390  libcuda1-390 nvidia-libopencl1-390 nvidia-opencl-icd-390 nvidia-settings bumblebee-nvidia primus virtualgl linux-headers-$(uname -r) primus-libs-ia32 virtualgl-libs-ia32
+sudo apt -y install bumblebee nvidia-390  libcuda1-390 nvidia-libopencl1-390 nvidia-opencl-icd-390 nvidia-settings bumblebee-nvidia primus virtualgl linux-headers-$(uname -r) primus-libs-ia32 virtualgl-libs-ia32
 
-cp /etc/bumblebee/bumblebee.conf /etc/bumblebee/bumblebee.conf.bac
-cp ./bumblebee.conf /etc/bumblebee
-cp /etc/default/grub /etc/default/grub.bac
-cp ./grub /etc/default/grub
-update-grub
+sudo cp /etc/bumblebee/bumblebee.conf /etc/bumblebee/bumblebee.conf.bac
+sudo cp ./bumblebee.conf /etc/bumblebee
+sudo cp /etc/default/grub /etc/default/grub.bac
+sudo cp ./grub /etc/default/grub
+sudo update-grub
 
-update-alternatives --set i386-linux-gnu_gl_conf /usr/lib/i386-linux-gnu/mesa/ld.so.conf
-update-alternatives --set x86_64-linux-gnu_egl_conf /usr/lib/x86_64-linux-gnu/mesa-egl/ld.so.conf
-update-alternatives --set x86_64-linux-gnu_gl_conf /usr/lib/x86_64-linux-gnu/mesa/ld.so.conf
+sudo update-alternatives --set i386-linux-gnu_gl_conf /usr/lib/i386-linux-gnu/mesa/ld.so.conf
+sudo update-alternatives --set x86_64-linux-gnu_egl_conf /usr/lib/x86_64-linux-gnu/mesa-egl/ld.so.conf
+sudo update-alternatives --set x86_64-linux-gnu_gl_conf /usr/lib/x86_64-linux-gnu/mesa/ld.so.conf
 
-dpkg-reconfigure bbswitch-dkms
-dpkg-reconfigure nvidia-390
-rm /etc/X11/xorg.conf
+sudo dpkg-reconfigure bbswitch-dkms
+sudo dpkg-reconfigure nvidia-390
+sudo rm /etc/X11/xorg.conf
 
-apt -y install mesa-utils
-apt install intel-microcode
-apt install -fy
-apt autoremove -y && apt clean
+sudo apt -y install mesa-utils
+sudo apt install intel-microcode
+sudo apt install -fy
+sudo apt autoremove -y
+sudo apt clean
 
 exit 0
